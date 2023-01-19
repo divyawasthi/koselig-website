170bccaf14db654058648af2b84e91698a22af0a
Divy Awasthi <94627370+divyawasthi@users.noreply.github.com>
2023-01-19 21:47:57 +0530

    Update Delicacies.js

--- src/components/Menu/Delicacies.js
+++ src/components/Menu/Delicacies.js
@@ -9,7 +9,7 @@ const Delicacies = () =>{
         <h2>Our Delicacies</h2>
             <ul className='del-menu'>
             <li className='li-food'><Link to='/Menu/Soul'>
-                <img className='food' src='https://scontent.fblr1-3.fna.fbcdn.net/v/t39.30808-6/277460519_133402359242194_2381358761536650115_n.jpg?stp=dst-jpg_s851x315&_nc_cat=109&ccb=1-7&_nc_sid=da31f3&_nc_ohc=eh2e_M50xbgAX8Y9ksC&_nc_ht=scontent.fblr1-3.fna&oh=00_AfBHKQNMdNlhykahWUWCWVcIEVuWXWw7KV5FivlYvta8Fw&oe=63C8646C'
+                <img className='food' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIQH49waX_-KXhT09Z5dF19nv292n22KbwZQ&usqp=CAU'
                 alt='something'/></Link>
                 <figcaption>Soul Food</figcaption>
             </li>
@@ -27,7 +27,7 @@ const Delicacies = () =>{
             </li>
             <li className='li-food'>
                 <Link to ='/Menu/Plates'>
-                <img className='food' src='https://scontent.fblr1-7.fna.fbcdn.net/v/t39.30808-6/284193008_150272494221847_9076553842616572802_n.jpg?stp=dst-jpg_s851x315&_nc_cat=106&ccb=1-7&_nc_sid=da31f3&_nc_ohc=7c0QLSrOmREAX9aKxAv&_nc_ht=scontent.fblr1-7.fna&oh=00_AfDxrrBH1kWKbN23WeuH56z4uIl8Mp_Kf2UToiM_TlYlPw&oe=63C7B580'
+                <img className='food' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbkGDKdt_lKgUzhsMw3SHurTAwMxMuMoxXoA&usqp=CAU'
                 alt='something'/></Link>
                 <figcaption>Little Plates</figcaption>
             </li>
